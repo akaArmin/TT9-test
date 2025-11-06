cae2d355d0952e3337169d908b4a67224e8a167f
akaArmin <130210549+akaArmin@users.noreply.github.com>
2025-11-06 11:06:51 +0100

    Update test.py to test 7seg

--- test/test.py
+++ test/test.py
@@ -17,7 +17,7 @@ async def test_project(dut):
     # Reset
     dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
+    dut.ui_in.value = 1
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
@@ -25,6 +25,33 @@ async def test_project(dut):
 
     dut._log.info("Test project behavior")
 
+    # the compare value is shifted 10 bits inside the design to allow slower counting
+      max_count = dut.ui_in.value << 10
+      dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
+      # check all segments and roll over
+      for i in range(15):
+          dut._log.info("check segment {}".format(i))
+          await ClockCycles(dut.clk, max_count)
+          # assert int(dut.segments.value) == segments[i % 10]
+      
+          # all bidirectionals are set to output
+          # assert dut.uio_oe == 0xFF
+    
+      # reset
+      dut.rst_n.value = 0
+      # set a different compare value
+      dut.ui_in.value = 3
+      await ClockCycles(dut.clk, 10)
+      dut.rst_n.value = 1
+    
+      max_count = dut.ui_in.value << 10
+      dut._log.info(f"check all segments with MAX_COUNT set to {max_count}")
+      # check all segments and roll over
+      for i in range(15):
+          dut._log.info("check segment {}".format(i))
+          await ClockCycles(dut.clk, max_count)
+          # assert int(dut.segments.value) == segments[i % 10]
+'''
     # Set the input values you want to test
     dut.ui_in.value = 20
     dut.uio_in.value = 30
@@ -38,3 +65,4 @@ async def test_project(dut):
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
+'''
